b1690da4e9d7f9ef486cf5cc5f28cde66fb924eb
Vladyslav Kovalchuk <vladkovalchuk09@gmail.com>
2025-03-15 18:26:37 +0200

    Тема 6. Форми. Домашня робота

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import ContactList from "./components/ContactList.jsx";
+import SearchBox from "./components/SearchBox.jsx";
+import ContactForm from "./components/ContactForm.jsx";
+import DemoDataButton from "./components/DemoDataButton.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem("contactData") ?? [];
+    return savedContacts.length ? JSON.parse(savedContacts) : [];
+  });
+  const [searchQuery, setQuery] = useState("");
+
+  useEffect(() => {
+    const componentData = JSON.stringify(contacts);
+    localStorage.setItem("contactData", componentData);
+  }, [contacts]);
+
+  const createContact = (contact) => {
+    setContacts((prevContacts) => [...prevContacts, contact]);
+  };
+
+  const deleteContact = (id) => {
+    const filteredContacts = contacts.filter((contact) => contact.id !== id);
+    setContacts(filteredContacts);
+  };
+
+  const searchResults = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(searchQuery)
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="mainView">
+      <h1>Phonebook</h1>
+      <ContactForm createContact={createContact} />
+      <SearchBox value={searchQuery} handleChange={setQuery} />
+      <ContactList contacts={searchResults} deleteContact={deleteContact} />
+      <DemoDataButton setContacts={setContacts} />
+    </div>
+  );
+};
 
-export default App
+export default App;
